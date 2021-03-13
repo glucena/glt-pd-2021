3c2e99c908e7dc25b032c1bdb9fbdaf7283647f0
glucena <gabriellucenatoro@gmail.com>
2021-03-13 15:07:32 +0100

    2. Registration & Login
    - Fix the tests

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,32 +1,29 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-
 
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-  let router: Router;
-  
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
-      imports:[         
-        FormsModule, 
+      imports: [
+        FormsModule,
+        HttpClientModule,
+        ReactiveFormsModule,
         RouterTestingModule.withRoutes([]),
-        ReactiveFormsModule],
-        
-
+      ],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
-   
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
